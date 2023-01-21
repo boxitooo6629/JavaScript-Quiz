9d39ab8ae8c721ed011df284594e0c2abe040841
Boxi Boxi <blago.dishkov@outlook.com>
2023-01-21 11:19:33 +0000

    new update

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,31 +1,47 @@
-var questionsTag = document.getElementById("questions")
+ //html elements
+let questionsTag = document.getElementById("questions");
 
-var timer = document.getElementById("time")
+let timer = document.getElementById("time");
 
-var questionTitle = document.getElementById("questions-title")
+let questionTitle = document.getElementById("questions-title");
 
-var questionChoises = document.getElementById("questions-choises")
+let questionChoises = document.getElementById("questions-choises");
+
+let startButton = document.getElementById("start");
+
+let submitButton = document.getElementById("submit");
+
+let initialElement = document.getElementById("initials");
+
+let feedBackElement = document.getElementById("feedback");
+
+let sfx = new Audio("assets/sfx/correct.wav");
+
+
+ 
+
+// var to keep track on time
+
+let currentQuestionIndex = 0;
+let time = questionsTag.lenght * 15;
+let timerID;
 
 
 
-var startButton = document.getElementById("start")
+function startQuiz() {
+        let startScreenElement = document.getElementById("start-screen");
+        startScreenElement.setAttribute("class", "hide");
 
+        questionsElements.removeAttributes("class");
 
-startButton.addEventListener("click", function (evt)  {
-    evt.preventDefault()
-    startScreen.setAttribute("class", "hide")
-    displayQuestion()
-} )
-
-function displayQuestion() {
-
+        timerID = setInterval(clockTick, 1000)
 
 }
 
 function timerLogic() {
-
-    var time = 100
-    var countdown = setInterval(function () {
+    start.addEventListeners("start");
+    let time = 100;
+    let countdown = setInterval(function () {
         if(time === 0) {
             clearInterval(countdown)
         }
@@ -35,3 +51,95 @@ function timerLogic() {
     },1000)
 }
 timerLogic()
+
+function quizEnd(){
+    clearInterval(timerID);
+
+    let endScreenElement = document.getElementByID("end-screen");
+    endScreenElement.removeAttributes("class");
+
+    let finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time;
+
+    questionsElement.setAttribute("class", "hide");
+}
+
+function questionClick() {
+    if(this.value != questions [currentQuestionIndex].answer) {
+        time -= 15;
+        if(time < 0) {
+            time = 0;
+        }
+        timerElement.textContent = time;
+
+        feedBackElement.textContent = "Wrong!"
+    } else {
+        sfxRight.play();
+        feedBackElement.textContent = "Correct!";
+    }
+
+    feedBackElement.setAttribute("class", "feedback");
+    
+    setTimeout(function(){ 
+        feedBackElement.setAttribute("class", "feedback hide")
+    }, 1000);
+
+    currentQuestionIndex++;
+
+    if(currentQuestionIndex === questions.lenght) {
+        quizEnd()
+    } else {
+        getQuestion();
+    }
+}
+
+function highScore() {
+    let initials = initialElement.value.trim();
+
+    if(initials !== ""){
+        let highScores = JSON.parse(localStorage.getItem("highscores")) || [] ;
+
+        let newScore ={
+            score: time,
+            initials: initials
+        }
+
+    }
+    highScore.push(newScore);
+    localStorage.setItem("highscore", JSON.stringify(highScores));
+
+    window.location.href = "highscore.html";
+
+}
+
+function checkForEnter(event){
+    if(event.key === "Enter") {
+        saveHighScore();
+
+    }
+}
+
+function getQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+
+    let titleElement = document.getElementById("question-title");
+
+    titleElement.textContent = currentQuestion.title;
+
+    choisesElement.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, index) {
+        let choiceBotton = document.createElement("button");
+
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+
+        choiceButton.textContent = `${index + 1}. ${choice}`
+
+        choiceButton.addEventListeners("click", questionClick);
+
+        choicesElement.append(choiceButton);
+
+    
+    })
+}
--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,6 +1,22 @@
 function highScore() {
 
-}
+    let highScore = JSON.parse(localStorage.getItem("highscores")) || [];
+
+    highScore.sort(function(a, b) {
+        return b.score - a.score;
+    })
+
+    highScore.forEach(function(score){
+        
+        let li = document.createElement("li");
+        li.textContent = `${score.initials} - ${score.score}`
+        let ol = document.getElementById("highscores");
+        ol.appendChild(li);
+    })
+        
+    }
+
+
 
 function removeHighScore() {
 
